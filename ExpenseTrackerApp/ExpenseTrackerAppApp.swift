fb2441b27e9723239662053f71852e3370e71e6a
Durga, Geethika (Contractor) <geethika_durga@comcast.com>
2026-03-18 10:20:09 +0530

    Initial commit: setup project

---
+++ ExpenseTrackerApp/ExpenseTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerAppApp.swift
+//  ExpenseTrackerApp
+//
+//  Created by mohana.g.durga on 18/03/26.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExpenseTrackerAppTests/ExpenseTrackerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerAppTests.swift
+//  ExpenseTrackerAppTests
+//
+//  Created by mohana.g.durga on 18/03/26.
+//
+
+import Testing
+@testable import ExpenseTrackerApp
+
+struct ExpenseTrackerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
